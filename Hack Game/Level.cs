ed3977af0bd79a7655ffe503ae42b858756f4fb1
NewCommander97 <timotte97@gmail.com>
2016-03-26 15:01:18 +0100

    Added Level Class

---
+++ Hack Game/Level.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hack_Game
+{
+    class Level
+    {
+        public Router MainRouter { get; set; }
+
+        public Network_Device StartDevice { get; set; }
+
+        public ChapterScreen ChapterScreen { get; set; }
+
+        public void Start()
+        {
+            ChapterScreen.ShowScreen();
+            switch (StartDevice.Devicetype)
+            {
+                case Network_Device.DeviceType.Client:
+                    Client c = (Client)StartDevice;
+                    c.OpenCommandPrompt();
+                    break;
+                case Network_Device.DeviceType.Router:
+                    Router r = (Router)StartDevice;
+                    r.OpenCommandPrompt();
+                    break;
+                case Network_Device.DeviceType.Server:
+                    Server s = (Server)StartDevice;
+                    s.OpenCommandPrompt();
+                    break;
+            }
+        }
+    }
+}
